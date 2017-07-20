336574d63c704c68f1985aeea2c31f94b688f66d
allan-mlpe <allan_mlpe@hotmail.com>
2017-07-20 16:02:53 -0300

    Inclusão dos resultados da análise de 127 cenários de 01-06-2014 a 15-06-2014

---
+++ public/web2py/rev_716b6e2_d06e4f6/unstructured-result/pt-br.py
@@ -0,0 +1,73 @@
+<<<<<<< MINE
+'About': 'Sobre',
+'Access Control': 'Controle de Acesso',
+'Administrative Interface': 'Interface Administrativa',
+=======
+'@markmin\x01An error occured, please [[reload %s]] the page': 'An error occured, please [[reload %s]] the page',
+'About': 'About',
+'Access Control': 'Access Control',
+'Administrative Interface': 'Administrative Interface',
+>>>>>>> YOURS
+
+##################################################################################################
+
+<<<<<<< MINE
+'Edit This App': 'Editar esta aplicaÃ§Ã£o',
+'Email and SMS': 'Email e SMS',
+'Errors': 'Erros',
+=======
+'Edit This App': 'Edit This App',
+'Email and SMS': 'Email and SMS',
+'Enter an integer between %(min)g and %(max)g': 'Enter an integer between %(min)g and %(max)g',
+'Errors': 'Errors',
+>>>>>>> YOURS
+
+##################################################################################################
+
+<<<<<<< MINE
+'lost password?': 'esqueceu a senha?',
+=======
+'Lost password?': 'Lost password?',
+'lost password?': 'lost password?',
+>>>>>>> YOURS
+
+##################################################################################################
+
+<<<<<<< MINE
+'Registration key': 'Chave de registro',
+'Reset Password key': 'Resetar chave de senha',
+'Resources': 'Recursos',
+'Role': 'Papel',
+=======
+'Registration identifier': 'Registration identifier',
+'Registration key': 'Registration key',
+'Reset Password key': 'Reset Password key',
+'Resources': 'Resources',
+'Role': 'Role',
+>>>>>>> YOURS
+
+##################################################################################################
+
+<<<<<<< MINE
+'The Views': 'As views',
+'This App': 'Esta aplicaÃ§Ã£o',
+=======
+'The Views': 'The Views',
+'This App': 'This App',
+'This email already has an account': 'This email already has an account',
+>>>>>>> YOURS
+
+##################################################################################################
+
+<<<<<<< MINE
+'You are successfully running web2py': 'VocÃª estÃ¡ rodando o web2py com sucesso',
+'You are successfully running web2py.': 'VocÃª estÃ¡ rodando o web2py com sucesso.',
+'You can modify this application and adapt it to your needs': 'VocÃª pode modificar esta aplicaÃ§Ã£o e adaptÃ¡-la para as suas necessidades',
+'You visited the url %s': 'VocÃª acessou a url %s',
+=======
+'Working...': 'Working...',
+'You are successfully running web2py': 'You are successfully running web2py',
+'You are successfully running web2py.': 'You are successfully running web2py.',
+'You can modify this application and adapt it to your needs': 'You can modify this application and adapt it to your needs',
+'You visited the url %s': 'You visited the url %s',
+>>>>>>> YOURS
